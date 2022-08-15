948c3fb92a58e2a0d01c4527d344920533c77262
Ruan Carlos Faccin <103553596+ruanfaccin@users.noreply.github.com>
2022-08-15 16:50:03 -0300

    Add files via upload

---
+++ VetorSemRepeticao/ExAulaVetorSemRepeticao/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+namespace ExAulaVetorSemRepeticao
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+
+            string[] nomes = new string[10];
+            string entrada;
+            bool encontrou;
+
+            int i = 0;
+            while (i < 10)
+            {
+
+                Console.Write($"Digite um nome [{i}]: ");
+                entrada = Console.ReadLine();
+
+                encontrou = false;
+
+                for (int j = 0; j < i; j++)
+                {
+                    if (nomes[j].ToUpper() == entrada.ToUpper())
+                    {
+                        encontrou = true;
+                        break;
+                    }
+                }
+
+                if (encontrou)
+                {
+                    Console.WriteLine("Nome digitado já existe! Digite outro!");
+                }
+                else
+                {
+                    nomes[i] = entrada;
+                    i++;
+                }
+            }
+
+            Console.WriteLine();
+
+            //a.compareto(b) -> retorna 0 se é igual a b, retorna < 0 se a é menor que b, e retrona > 0 se a é maior que b
+
+            for (int k = 0; k < nomes.Length - 1; k++)
+            {
+                for (int l = k + 1; l < nomes.Length; l++)
+                {
+                    if (nomes[k].CompareTo(nomes[l]) > 0)
+                    {
+                        string aux = nomes[k];
+                        nomes[k] = nomes[l];
+                        nomes[l] = aux;
+                    }
+                }
+            }
+
+            for(int k = 0; k < nomes.Length; k++)
+            {
+                Console.WriteLine(nomes[k]);
+            }
+
+            Console.Write("Digite um nome para ser procurado: ");
+            string entradas = Console.ReadLine();
+
+            Console.WriteLine();
+            bool encontrado = false;
+
+            for (int j = 0; j < i; j++)
+            {
+                if (nomes[j].ToUpper() == entradas.ToUpper())
+                {
+                    encontrado = true;
+                }
+
+                if (encontrado)
+                {
+                    Console.WriteLine($"O nome está no indíce {j}");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("O nome digitado não existe!");
+                    break;
+                }
+            }
+            
+        }
+    }
+}
---
+++ WhileRepeat/ExCurso2While/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ExCurso2While
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string coor;
+
+            Console.Write("Digite as coordenadas: ");
+            coor = Console.ReadLine();
+
+            string[] vet = coor.Split(' ');
+            int vet1 = int.Parse(vet[0]);
+            int vet2 = int.Parse(vet[1]);
+
+            while (vet1 != 0 && vet2 != 0)
+            {
+
+                if (vet1 > 0 && vet2 > 0)
+                {
+                    Console.WriteLine("Está no primeiro quadrante!");
+                }
+                else if (vet1 < 0 && vet2 > 0)
+                {
+                    Console.WriteLine("Está no segundo quadrante!");
+                }
+                else if (vet1 < 0 && vet2 < 0)
+                {
+                    Console.WriteLine("Está no terceiro quadrante!");
+                }
+                else
+                {
+                    Console.WriteLine("Está no quarto quadrante!");
+                }
+
+                vet = Console.ReadLine().Split(' ');
+                vet1 = int.Parse(vet[0]);
+                vet2 = int.Parse(vet[1]);
+
+                Console.WriteLine("Programa encerrado!");
+
+            }
+        }
+    }
+}
