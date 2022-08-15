ef2329151f0fe6407225c9464bb60bc18fb7be09
Ruan Carlos Faccin <103553596+ruanfaccin@users.noreply.github.com>
2022-08-15 16:45:53 -0300

    Add files via upload

---
+++ Encapsulamento/ExCursoEncapsulamento/ContaBancaria.cs
@@ -0,0 +1,59 @@
+﻿using System;
+namespace ExCursoEncapsulamento
+{
+    public class ContaBancaria
+    {
+
+        public string Nome { get; set; } 
+        public int NConta { get; set; }
+        public double Saque { get; set; }
+        public double DepositoInicial { get; set; }
+        public double Deposito {get; set; }
+        public char Sim;
+
+ 
+        public ContaBancaria ()
+        {
+
+        }
+
+        public ContaBancaria(int nConta)
+        {
+            NConta = nConta;
+          
+
+        }
+
+        public ContaBancaria(string nome, double saque, double depositoInicial, double deposito)
+        {
+            Nome = nome;
+            Saque = saque;
+            DepositoInicial = depositoInicial;
+            Deposito = deposito;
+        }
+
+        private double ValorConta ()
+        {
+            if (Saque != 0)
+            {
+                return 0 + DepositoInicial + Deposito - Saque - 5;
+            }
+            else
+            {
+                return 0 + DepositoInicial + Deposito;
+
+
+            }
+        }
+
+
+
+        public string Conta()
+        {
+            return $"Titular: "+ Nome +
+                ", Conta: " + NConta + ", Saldo: R$" + ValorConta();
+        } 
+
+
+    }
+}
---
+++ Encapsulamento/ExCursoEncapsulamento/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExCursoEncapsulamento
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            ContaBancaria Pessoa;
+
+            Pessoa = new ContaBancaria();
+
+            Console.Write("Entre com o número da conta: ");
+            Pessoa.NConta = int.Parse(Console.ReadLine());
+
+            Console.Write("Entre com o nome da usuário: ");
+            Pessoa.Nome = Console.ReadLine();
+
+            Console.Write("Haverá depósito inicial? (s/n) ");
+            Pessoa.Sim = Char.Parse(Console.ReadLine());
+
+            if (Pessoa.Sim == 's')
+            {
+                Console.Write("Entre com o valor do depósito inicial: ");
+                Pessoa.DepositoInicial = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Dados da conta: ");
+            Console.WriteLine(Pessoa.Conta(), CultureInfo.InvariantCulture);
+
+            Console.Write("Entre com um valor para depósito: ");
+            Pessoa.Deposito = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados da conta atualizados: ");
+            Console.WriteLine(Pessoa.Conta(), CultureInfo.InvariantCulture);
+
+            Console.Write("Entre com um valor para saque: ");
+            Pessoa.Saque = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados da conta atualizados: ");
+            Console.WriteLine(Pessoa.Conta(), CultureInfo.InvariantCulture);
+
+
+        }
+    }
+}
